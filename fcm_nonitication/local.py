8aa245d2796403b800fac51880e0088628a0a562
Ramji Subedi <ramjisubedi1991@gmail.com>
2023-03-19 15:15:13 +0545

    FCM config setup

---
+++ fcm_nonitication/local.py
@@ -0,0 +1,10 @@
+
+cred = 'Your servive account file path'
+
+FCM_DJANGO_SETTINGS = {
+    "FCM_SERVER_KEY": "your key",
+    # true if you want to have only one active device per registered user at a time
+    # default: False
+}
+
+FCM_API_KEY="your key"
